af964ceef5a67c4a6116ca576c0dfac14bb9964b
nadineRoldao <vitorianadinecarlos@gmail.com>
2023-10-04 16:31:13 -0300

    creating tasks system frontend base

---
+++ tasks-system-frontend/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <h1> tasks system frontend is working </h1>
+  `,
+  styles: []
+})
+export class AppComponent {
+  
+}
