14f3cab1e196986840c424cc0fd2cbbbaf624ef4
nadineRoldao <vitorianadinecarlos@gmail.com>
2023-10-04 08:31:27 -0300

    backend project structure has been created

---
+++ tasks-system-backend/src/main/java/com/nadinetest/taskssystem/TaskssystemApplication.java
@@ -0,0 +1,13 @@
+package com.nadinetest.taskssystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskssystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskssystemApplication.class, args);
+	}
+
+}
---
+++ tasks-system-backend/src/test/java/com/nadinetest/taskssystem/TaskssystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nadinetest.taskssystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskssystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
